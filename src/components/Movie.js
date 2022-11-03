fbb8200df73f4a5fba7d29f7fdae042d515f2c3e
akmaljon <akmaljon.ahmedjanov@yandex.ru>
2022-11-03 19:00:39 +0500

    id remove

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,12 +3,12 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster, genres }) {
+function Movie ({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
       <Link
-        to={{
-          pathname: `/movie/${id}`,
+        to={ {
+          pathname: `/movie/${ id }`,
           state: {
             year,
             title,
@@ -16,22 +16,22 @@ function Movie({ id, year, title, summary, poster, genres }) {
             poster,
             genres,
           },
-        }}
+        } }
       >
-        <img src={poster} alt={title} title={title} />
+        <img src={ poster } alt={ title } title={ title } />
         <div className="movie__column">
-          <h3 className="movie__title">{title}</h3>
-          <h5 className="movie__year">{year}</h5>
+          <h3 className="movie__title">{ title }</h3>
+          <h5 className="movie__year">{ year }</h5>
           <ul className="movie__genres">
-            {genres.map((genre, index) => {
+            { genres.map((genre, index) => {
               return (
-                <li key={index} className="genres__genre">
-                  {genre}
+                <li key={ index } className="genres__genre">
+                  { genre }
                 </li>
               );
-            })}
+            }) }
           </ul>
-          <p className="movie__summary">{summary.slice(0, 140)}...</p>
+          <p className="movie__summary">{ summary.slice(0, 140) }...</p>
         </div>
       </Link>
     </div>
